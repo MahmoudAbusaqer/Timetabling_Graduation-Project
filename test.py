f6237c07392ce360b2145011d9817c7749434e26
MahmoudAbusaqer <mahmoudabusaqer1@gmail.com>
2022-05-12 22:55:39 +0300

    Changed the code to meet and add the IUG input data, and translated the attributes to meet and get the data from the input data file

--- test.py
+++ test.py
@@ -1,16 +1,18 @@
 import json
 import random
 
+from numpy import number
+
 
 def load_data(path):
     with open(path, 'r') as read_file:
         data = json.load(read_file)
 
-    for university_class in data['Casovi']:
-        classroom = university_class['Ucionica']
-        university_class['Ucionica'] = data['Ucionice'][classroom]
+    for university_class in data['Classes']:
+        classroom = university_class['Classroom']
+        university_class['Classroom'] = data['Classrooms'][classroom]
 
-    data = data['Casovi']
+    data = data['Classes']
 
     return data
 
@@ -23,37 +25,41 @@ def generate_chromosome(data):
 
     new_data = []
 
+    number_of_runs = 0
+
     # reserve 60 place for the hole timetable to put the random generated value in it
     for single_class in data:
-        professors[single_class['Nastavnik']] = [0] * 40
-        for classroom in single_class['Ucionica']:
+        professors[single_class['Lecturer']] = [0] * 40
+        for classroom in single_class['Classroom']:
             classrooms[classroom] = [0] * 40
-        for group in single_class['Grupe']:
+        for group in single_class['Group']:
             groups[group] = [0] * 40
-        subjects[single_class['Predmet']] = {'P': [], 'V': [], 'L': []}  # need edit (2 needed)
+        subjects[single_class['Subject']] = {'P': [], 'V': [], 'L': []}  # need edit (2 needed)
 
     for single_class in data:
         new_single_class = single_class.copy()
-        classroom = random.choice(single_class['Ucionica'])  # get a random classroom from the classrooms for this class
+        classroom = random.choice(single_class['Classroom'])  # get a random classroom from the classrooms for this class
         day = random.randrange(0, 5)  # get a random day for this class
-        period = random.randrange(0, 9 - int(single_class['Trajanje']))  # get a random period (time) for this class
-        print("period = " + str(period))
-        # create a new variable called zadata_ucionica to store the initial assigned classroom to the class
-        new_single_class['Zadata_ucionica'] = classroom
+        period = random.randrange(0, 9 - int(single_class['Duration']))  # get a random period (time) for this class
+        # print("period = " + str(period))
+        # create a new variable called assigned_classroom (zadata_ucionica) to store the initial assigned classroom to the class
+        new_single_class['assigned_classroom'] = classroom
         all_times = []  # To store the times reserved for this class
-        class_length = single_class['Trajanje']
+        class_length = single_class['Duration']
 
-        reserve_professor = professors[new_single_class['Nastavnik']]
+        reserve_professor = professors[new_single_class['Lecturer']]
         reserve_classroom = classrooms[classroom]
         reserve_group = groups
 
         # Saturday, Monday and Wednesday classes
         if day == 0 or day == 2 or day == 4:
             # reserve 1 time for the lab classes
-            if single_class['Trajanje'] == "2" and single_class['Tip'] == 'L':
+            if single_class['Duration'] == "2" and single_class['Type'] == 'V':
                 print("***state 1***")
+                # print(str(single_class['Predmet']))
+                # print(str(single_class['Tip']))
                 time = 8 * day + period
-                new_single_class['Zadato_vreme'] = time
+                new_single_class['assigned_time'] = time
                 reserve_time(time, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class, subjects)
                 # for i in range(time, time + int(single_class['Trajanje'])):
                 #     professors[new_single_class['Nastavnik']][i] += 1  # there is a problem here IndexError: list index out of range
@@ -62,8 +68,10 @@ def generate_chromosome(data):
                 #         groups[group][i] += 1
                 # subjects[new_single_class['Predmet']][new_single_class['Tip']].append((time, new_single_class['Grupe']))
             # reserve 3 times for the lecture classes with a Length of 3 hours
-            elif class_length == "3" and single_class['Tip'] == 'P':
+            elif class_length == "3" and single_class['Type'] == 'P':
                 print("***state 2***")
+                # print(str(single_class['Predmet']))
+                # print(str(single_class['Tip']))
                 # elif (single_class['Trajanje'] == 3 or single_class['Trajanje'] == 2) and single_class['Tip'] == 'P':
                 # all_times = []
                 time = 8 * 0 + period
@@ -72,7 +80,7 @@ def generate_chromosome(data):
                 all_times.append(time)
                 all_times.append(time2)
                 all_times.append(time3)
-                new_single_class['Zadato_vreme'] = all_times
+                new_single_class['assigned_time'] = all_times
                 # for i in range(time, time + int(single_class['Trajanje'])):
                 #     professors[new_single_class['Nastavnik']][i] += 1
                 #     classrooms[classroom][i] += 1
@@ -96,20 +104,21 @@ def generate_chromosome(data):
                 reserve_time(time3, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class, subjects)
 
             # reserve 2 times for the lecture classes with a Length of 2 hours
-            elif single_class['Trajanje'] == "2" and single_class['Tip'] == 'P':
+            elif single_class['Duration'] == "2" and single_class['Type'] == 'P':
                 print("***state 3***")
-
+                # print(str(single_class['Predmet']))
+                # print(str(single_class['Tip']))
                 # elif (single_class['Trajanje'] == 3 or single_class['Trajanje'] == 2) and single_class['Tip'] == 'P':
                 # all_times = []
                 time = 8 * 0 + period
                 time2 = 8 * 4 + period
-                print("1 - time = " + str(time))
-                print("1 - time2 = " + str(time2))
-                print("1- time + int(single_class['Trajanje']) = " + str(time + int(single_class['Trajanje'])))
+                # print("1 - time = " + str(time))
+                # print("1 - time2 = " + str(time2))
+                # print("1- time + int(single_class['Trajanje']) = " + str(time + int(single_class['Trajanje'])))
 
                 all_times.append(time)
                 all_times.append(time2)
-                new_single_class['Zadato_vreme'] = all_times
+                new_single_class['assigned_time'] = all_times
                 # for i in range(time, time + int(single_class['Trajanje'])):
                 #     increment = 0
                 #     print("i = " + str(i))
@@ -152,10 +161,12 @@ def generate_chromosome(data):
         # Sunday and Thursday classes
         elif day == 1 or day == 3:
             # reserve 1 time for the lab classes
-            if class_length == "2" and single_class['Tip'] == 'L':
+            if class_length == "2" and single_class['Type'] == 'V':
                 print("***state 4***")
+                # print(str(single_class['Predmet']))
+                # print(str(single_class['Tip']))
                 time = 8 * day + period
-                new_single_class['Zadato_vreme'] = time
+                new_single_class['assigned_time'] = time
                 # for i in range(time, time + int(single_class['Trajanje'])):
                 #     professors[new_single_class['Nastavnik']][i] += 1
                 #     classrooms[classroom][i] += 1
@@ -165,15 +176,17 @@ def generate_chromosome(data):
                 reserve_time(time, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class, subjects)
 
             # reserve multiple times (2 times) for a lecture classes
-            elif class_length in ("2", "3") and single_class['Tip'] == 'P':
+            elif class_length in ("2", "3") and single_class['Type'] == 'P':
                 print("***state 5***")
+                # print(str(single_class['Predmet']))
+                # print(str(single_class['Tip']))
                 # elif (single_class['Trajanje'] == 3 or single_class['Trajanje'] == 2) and single_class['Tip'] == 'P':
                 # all_times = []
                 time = 8 * 1 + period
                 time2 = 8 * 3 + period
                 all_times.append(time)
                 all_times.append(time2)
-                new_single_class['Zadato_vreme'] = all_times
+                new_single_class['assigned_time'] = all_times
                 # for i in range(time, time + int(single_class['Trajanje'])):
                 #     professors[new_single_class['Nastavnik']][i] += 1
                 #     classrooms[classroom][i] += 1
@@ -190,7 +203,9 @@ def generate_chromosome(data):
                 reserve_time(time2, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class, subjects)
 
         new_data.append(new_single_class)
-
+        print(str(new_single_class))
+        number_of_runs += 1
+        print(str(number_of_runs))
     return new_data, professors, classrooms, groups, subjects
 
 
@@ -201,7 +216,7 @@ def reserve_time(time, class_length, reserve_professor, reserve_classroom, reser
         reserve_classroom[i] += 1
         for group in reserve_group:
             reserve_group[group][i] += 1
-    subjects[new_single_class['Predmet']][new_single_class['Tip']].append((time, new_single_class['Grupe']))
+    subjects[new_single_class['Subject']][new_single_class['Type']].append((time, new_single_class['Group']))
 
 
 def write_data(data, path):
@@ -210,8 +225,8 @@ def write_data(data, path):
 
 
 if __name__ == "__main__":
-    input_file = 'classes/input2.json'
-    output_file = 'classes/output5.json'
+    input_file = 'classes/iug_input1.json'
+    output_file = 'classes/iug_output.json'
     da = load_data(input_file)
     chromosome = generate_chromosome(da)
     write_data(chromosome[0], output_file)
