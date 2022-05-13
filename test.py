b84f62d4d6731f637ceb402350f6ab11d1d32862
MahmoudAbusaqer <mahmoudabusaqer1@gmail.com>
2022-05-14 01:37:17 +0300

    Changed the chromosome from 40 Genes to 80 Genes to meet the IUG classes time that has a one and a half hour

--- test.py
+++ test.py
@@ -25,18 +25,20 @@ def generate_chromosome(data):
 
     number_of_runs = 0
 
-    # reserve 60 place for the hole timetable to put the random generated value in it
+    # create the chromosome which will reserve 80 places (Genes) for the whole timetable to put the randomly generated
+    # value in it, every place consists of a half-hour (Gene)
     for single_class in data:
-        professors[single_class['Lecturer']] = [0] * 40
+        professors[single_class['Lecturer']] = [0] * 80
         for classroom in single_class['Classroom']:
-            classrooms[classroom] = [0] * 40
+            classrooms[classroom] = [0] * 80
         for group in single_class['Group']:
-            groups[group] = [0] * 40
+            groups[group] = [0] * 80
         subjects[single_class['Subject']] = {'P': [], 'V': [], 'L': []}  # need edit (2 needed)
 
     for single_class in data:
         new_single_class = single_class.copy()
-        classroom = random.choice(single_class['Classroom'])  # get a random classroom from the classrooms for this class
+        classroom = random.choice(
+            single_class['Classroom'])  # get a random classroom from the classrooms for this class
         day = random.randrange(0, 5)  # get a random day for this class
         period = random.randrange(0, 9 - int(single_class['Duration']))  # get a random period (time) for this class
 
@@ -54,67 +56,89 @@ def generate_chromosome(data):
             # reserve 1 time for the lab classes
             if single_class['Duration'] == "2" and single_class['Type'] == 'V':
                 print("***state 1***")
-
-                time = 8 * day + period
+                time = 16 * day + period
                 new_single_class['assigned_time'] = time
-                reserve_time(time, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class, subjects)
+                reserve_time(time, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class,
+                             subjects)
 
             # reserve 3 times for the lecture classes with a Length of 3 hours
             elif class_length == "3" and single_class['Type'] == 'P':
                 print("***state 2***")
 
-                time = 8 * 0 + period
-                time2 = 8 * 2 + period
-                time3 = (8 * 4) + period
+                time = 16 * 0 + period
+                time2 = 16 * 2 + period
+                time3 = (16 * 4) + period
                 all_times.append(time)
                 all_times.append(time2)
                 all_times.append(time3)
                 new_single_class['assigned_time'] = all_times
 
-                reserve_time(time, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class, subjects)
-                reserve_time(time2, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class, subjects)
-                reserve_time(time3, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class, subjects)
+                reserve_time(time, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class,
+                             subjects)
+                reserve_time(time2, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class,
+                             subjects)
+                reserve_time(time3, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class,
+                             subjects)
 
             # reserve 2 times for the lecture classes with a Length of 2 hours
             elif single_class['Duration'] == "2" and single_class['Type'] == 'P':
                 print("***state 3***")
 
-                time = 8 * 0 + period
-                time2 = 8 * 4 + period
+                time = 16 * 0 + period
+                time2 = 16 * 4 + period
 
                 all_times.append(time)
                 all_times.append(time2)
                 new_single_class['assigned_time'] = all_times
 
-                reserve_time(time, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class, subjects)
-                reserve_time(time2, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class, subjects)
+                reserve_time(time, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class,
+                             subjects)
+                reserve_time(time2, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class,
+                             subjects)
+
+            # reserve 1 time for a lecture class that is one hour only
+            elif class_length == "1" and single_class['Type'] == 'P':
+                print("***state 6***")
+                time = 16 * day + period
+                new_single_class['assigned_time'] = time
+                reserve_time(time, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class,
+                             subjects)
 
         # Sunday and Thursday classes
         elif day == 1 or day == 3:
             # reserve 1 time for the lab classes
             if class_length == "2" and single_class['Type'] == 'V':
                 print("***state 4***")
-
-                time = 8 * day + period
+                time = 16 * day + period
                 new_single_class['assigned_time'] = time
-
-                reserve_time(time, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class, subjects)
+                reserve_time(time, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class,
+                             subjects)
 
             # reserve multiple times (2 times) for a lecture classes
             elif class_length in ("2", "3") and single_class['Type'] == 'P':
                 print("***state 5***")
 
-                time = 8 * 1 + period
-                time2 = 8 * 3 + period
+                time = 16 * 1 + period
+                time2 = 16 * 3 + period
                 all_times.append(time)
                 all_times.append(time2)
                 new_single_class['assigned_time'] = all_times
 
-                reserve_time(time, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class, subjects)
-                reserve_time(time2, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class, subjects)
+                reserve_time(time, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class,
+                             subjects)
+                reserve_time(time2, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class,
+                             subjects)
+
+            # reserve 1 time for a lecture class that is one hour only
+            elif class_length == "1" and single_class['Type'] == 'P':
+                print("***state 6***")
+                time = 16 * day + period
+                new_single_class['assigned_time'] = time
+                reserve_time(time, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class,
+                             subjects)
 
         new_data.append(new_single_class)
-        print(str(new_single_class))
+        # print(str(new_single_class))
         number_of_runs += 1
         print(str(number_of_runs))
     return new_data, professors, classrooms, groups, subjects
