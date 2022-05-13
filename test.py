f07aeed8dfa3dd8b141a007b0209d846021ff4b1
MahmoudAbusaqer <mahmoudabusaqer1@gmail.com>
2022-05-13 21:10:44 +0300

    Removed the unwanted code

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 import json
 import random
 
-from numpy import number
-
 
 def load_data(path):
     with open(path, 'r') as read_file:
@@ -41,7 +39,7 @@ def generate_chromosome(data):
         classroom = random.choice(single_class['Classroom'])  # get a random classroom from the classrooms for this class
         day = random.randrange(0, 5)  # get a random day for this class
         period = random.randrange(0, 9 - int(single_class['Duration']))  # get a random period (time) for this class
-        # print("period = " + str(period))
+
         # create a new variable called assigned_classroom (zadata_ucionica) to store the initial assigned classroom to the class
         new_single_class['assigned_classroom'] = classroom
         all_times = []  # To store the times reserved for this class
@@ -56,24 +54,15 @@ def generate_chromosome(data):
             # reserve 1 time for the lab classes
             if single_class['Duration'] == "2" and single_class['Type'] == 'V':
                 print("***state 1***")
-                # print(str(single_class['Predmet']))
-                # print(str(single_class['Tip']))
+
                 time = 8 * day + period
                 new_single_class['assigned_time'] = time
                 reserve_time(time, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class, subjects)
-                # for i in range(time, time + int(single_class['Trajanje'])):
-                #     professors[new_single_class['Nastavnik']][i] += 1  # there is a problem here IndexError: list index out of range
-                #     classrooms[classroom][i] += 1
-                #     for group in new_single_class['Grupe']:
-                #         groups[group][i] += 1
-                # subjects[new_single_class['Predmet']][new_single_class['Tip']].append((time, new_single_class['Grupe']))
+
             # reserve 3 times for the lecture classes with a Length of 3 hours
             elif class_length == "3" and single_class['Type'] == 'P':
                 print("***state 2***")
-                # print(str(single_class['Predmet']))
-                # print(str(single_class['Tip']))
-                # elif (single_class['Trajanje'] == 3 or single_class['Trajanje'] == 2) and single_class['Tip'] == 'P':
-                # all_times = []
+
                 time = 8 * 0 + period
                 time2 = 8 * 2 + period
                 time3 = (8 * 4) + period
@@ -81,24 +70,7 @@ def generate_chromosome(data):
                 all_times.append(time2)
                 all_times.append(time3)
                 new_single_class['assigned_time'] = all_times
-                # for i in range(time, time + int(single_class['Trajanje'])):
-                #     professors[new_single_class['Nastavnik']][i] += 1
-                #     classrooms[classroom][i] += 1
-                #     for group in new_single_class['Grupe']:
-                #         groups[group][i] += 1
-                # subjects[new_single_class['Predmet']][new_single_class['Tip']].append((time, new_single_class['Grupe']))
-                # for i in range(time2, time2 + int(single_class['Trajanje'])):
-                #     professors[new_single_class['Nastavnik']][i] += 1
-                #     classrooms[classroom][i] += 1
-                #     for group in new_single_class['Grupe']:
-                #         groups[group][i] += 1
-                # subjects[new_single_class['Predmet']][new_single_class['Tip']].append((time, new_single_class['Grupe']))
-                # for i in range(time3, time3 + int(single_class['Trajanje'])):
-                #     professors[new_single_class['Nastavnik']][i] += 1  #  problem here
-                #     classrooms[classroom][i] += 1
-                #     for group in new_single_class['Grupe']:
-                #         groups[group][i] += 1
-                # subjects[new_single_class['Predmet']][new_single_class['Tip']].append((time, new_single_class['Grupe']))
+
                 reserve_time(time, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class, subjects)
                 reserve_time(time2, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class, subjects)
                 reserve_time(time3, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class, subjects)
@@ -106,55 +78,14 @@ def generate_chromosome(data):
             # reserve 2 times for the lecture classes with a Length of 2 hours
             elif single_class['Duration'] == "2" and single_class['Type'] == 'P':
                 print("***state 3***")
-                # print(str(single_class['Predmet']))
-                # print(str(single_class['Tip']))
-                # elif (single_class['Trajanje'] == 3 or single_class['Trajanje'] == 2) and single_class['Tip'] == 'P':
-                # all_times = []
+
                 time = 8 * 0 + period
                 time2 = 8 * 4 + period
-                # print("1 - time = " + str(time))
-                # print("1 - time2 = " + str(time2))
-                # print("1- time + int(single_class['Trajanje']) = " + str(time + int(single_class['Trajanje'])))
 
                 all_times.append(time)
                 all_times.append(time2)
                 new_single_class['assigned_time'] = all_times
-                # for i in range(time, time + int(single_class['Trajanje'])):
-                #     increment = 0
-                #     print("i = " + str(i))
-                #     print("time2 + i = " + str(time2))
-                #     professors[new_single_class['Nastavnik']][i] += 1
-                #     print("before -> professors[new_single_class['Nastavnik']][time2 + i]")
-                #
-                #     x = time2 + increment
-                #     print("x" + str(x))
-                #     professors[new_single_class['Nastavnik']][x] += 1
-                #     print("after -> professors[new_single_class['Nastavnik']][time2 + i] ")
-                #     # المشكلة انه لما يجي يضيف بضيف في جين خارج حدود الكرموسوم
-                #     # حل المشكلة بانه لما يجي يختار الوقت ويضيف في الجين انه يضيف ايضا مجموع الساعات لهذا اليوم
-                #     # مثلا ليوم الاحد لازم نجمع الوقت + يوم السبت كله -> الوقت = 5 و اليوم الاحد -> 5 + 8 = 13
-                #     classrooms[classroom][i] += 1
-                #     classrooms[classroom][x] += 1
-                #     for group in new_single_class['Grupe']:
-                #         groups[group][i] += 1
-                #         groups[group][x] += 1
-                #
-                #     increment += 1
-                #     x = time2 + increment
-                #     print("x after = " + str(x))
-                # subjects[new_single_class['Predmet']][new_single_class['Tip']].append((time, new_single_class['Grupe']))
-                # subjects[new_single_class['Predmet']][new_single_class['Tip']].append(
-                #     (time2, new_single_class['Grupe']))
-                # print("time2 = " + str(time2))
-                # print("time2 + int(single_class['Trajanje']) = " + str(time2 + int(single_class['Trajanje'])))
-                # for j in range(time2, time2 + int(single_class['Trajanje'])):
-                #     print("j = " + str(j))
-                #     professors[new_single_class['Nastavnik']][j] += 1
-                #     print("professors[new_single_class['Nastavnik']][time2 + j] = " + str(professors[new_single_class['Nastavnik']][j]))
-                #     classrooms[classroom][j] += 1
-                #     for group in new_single_class['Grupe']:
-                #         groups[group][j] += 1
-                # subjects[new_single_class['Predmet']][new_single_class['Tip']].append((time2, new_single_class['Grupe']))
+
                 reserve_time(time, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class, subjects)
                 reserve_time(time2, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class, subjects)
 
@@ -163,42 +94,22 @@ def generate_chromosome(data):
             # reserve 1 time for the lab classes
             if class_length == "2" and single_class['Type'] == 'V':
                 print("***state 4***")
-                # print(str(single_class['Predmet']))
-                # print(str(single_class['Tip']))
+
                 time = 8 * day + period
                 new_single_class['assigned_time'] = time
-                # for i in range(time, time + int(single_class['Trajanje'])):
-                #     professors[new_single_class['Nastavnik']][i] += 1
-                #     classrooms[classroom][i] += 1
-                #     for group in new_single_class['Grupe']:
-                #         groups[group][i] += 1
-                # subjects[new_single_class['Predmet']][new_single_class['Tip']].append((time, new_single_class['Grupe']))
+
                 reserve_time(time, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class, subjects)
 
             # reserve multiple times (2 times) for a lecture classes
             elif class_length in ("2", "3") and single_class['Type'] == 'P':
                 print("***state 5***")
-                # print(str(single_class['Predmet']))
-                # print(str(single_class['Tip']))
-                # elif (single_class['Trajanje'] == 3 or single_class['Trajanje'] == 2) and single_class['Tip'] == 'P':
-                # all_times = []
+
                 time = 8 * 1 + period
                 time2 = 8 * 3 + period
                 all_times.append(time)
                 all_times.append(time2)
                 new_single_class['assigned_time'] = all_times
-                # for i in range(time, time + int(single_class['Trajanje'])):
-                #     professors[new_single_class['Nastavnik']][i] += 1
-                #     classrooms[classroom][i] += 1
-                #     for group in new_single_class['Grupe']:
-                #         groups[group][i] += 1
-                # subjects[new_single_class['Predmet']][new_single_class['Tip']].append((time, new_single_class['Grupe']))
-                # for i in range(time2, time2 + int(single_class['Trajanje'])):
-                #     professors[new_single_class['Nastavnik']][i] += 1
-                #     classrooms[classroom][i] += 1
-                #     for group in new_single_class['Grupe']:
-                #         groups[group][i] += 1
-                # subjects[new_single_class['Predmet']][new_single_class['Tip']].append((time, new_single_class['Grupe']))
+
                 reserve_time(time, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class, subjects)
                 reserve_time(time2, class_length, reserve_professor, reserve_classroom, reserve_group, new_single_class, subjects)
 
