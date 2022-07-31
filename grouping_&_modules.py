f7623d1f80dc172c36c29266848f2b47e461e808
MahmoudAbusaqer <mahmoudabusaqer1@gmail.com>
2022-08-01 00:57:56 +0300

    updated and some small changes to grouping_&_modules.py

--- grouping_&_modules.py
+++ grouping_&_modules.py
@@ -20,15 +20,26 @@ def load_data(path):
 def generate_department_groups(classes_data, departments_data):
     new_data = []
     departments = {}
+    depart_group = []
     for single_department in departments_data:
         department = departments_data[single_department]
-        department_groups = math.ceil(department / 20)
+        department_groups = math.ceil(department / 22)
         dep_group = {}
         for i in range(1, department_groups + 1):
             if single_department[-1] == "m":
-                dep_group[f"10{i}"] = []
+                if i.__lt__(10):
+                    dep_group[f"10{i}"] = []
+                    depart_group.append(f"{single_department} 10{i}")
+                elif i.__gt__(9):
+                    dep_group[f"1{i}"] = []
+                    depart_group.append(f"{single_department} 1{i}")
             elif single_department[-1] == "f":
-                dep_group[f'20{i}'] = []
+                if i.__lt__(10):
+                    dep_group[f'20{i}'] = []
+                    depart_group.append(f"{single_department} 20{i}")
+                elif i.__gt__(9):
+                    dep_group[f'2{i}'] = []
+                    depart_group.append(f"{single_department} 2{i}")
         departments[single_department] = dep_group
 
     for single_class in classes_data:
@@ -53,9 +64,9 @@ def generate_department_groups(classes_data, departments_data):
             for y in dept_groups:
                 group_y.append(y)
                 groups_to_sections[num_incre] = group_y
-                students_number_in_group += 20
-                total_number_of_students2 += 20
-                if students_number_in_group == class_capacity or (students_number_in_group + 20) > class_capacity \
+                students_number_in_group += 22
+                total_number_of_students2 += 22
+                if students_number_in_group == class_capacity or (students_number_in_group + 22) > class_capacity \
                         or (((total_number_of_students - total_number_of_students2) < class_capacity)
                             and dept_groups[-1] == y):
                     num_incre += 1
@@ -66,13 +77,21 @@ def generate_department_groups(classes_data, departments_data):
                 new_single_class = single_class.copy()
                 new_single_class['For_Group'] = groups_to_sections[i]
                 if new_single_class['Subject'][-1] == "M":
-                    new_single_class['Group'] = f"10{i}"
+                    if i.__lt__(10):
+                        new_single_class['Group'] = f"10{i}"
+                    elif i.__gt__(9):
+                        new_single_class['Group'] = f"1{i}"
                 elif new_single_class['Subject'][-1] == "F":
-                    new_single_class['Group'] = f"20{i}"
+                    if i.__lt__(10):
+                        new_single_class['Group'] = f"20{i}"
+                    elif i.__gt__(9):
+                        new_single_class['Group'] = f"2{i}"
 
-                lecturer = new_single_class['Lecturer']
-                number_of_lecturer = random.randrange(0, len(lecturer))
-                new_single_class['Lecturer'] = [lecturer[number_of_lecturer]]
+                # need to remove
+                # lecturer = new_single_class['Lecturer']
+                # number_of_lecturer = random.randrange(0, len(lecturer))
+                # new_single_class['Lecturer'] = [lecturer[number_of_lecturer]]
+
                 for j in groups_to_sections[i]:
                     new_pair = j.rsplit(" ")
                     departments[new_pair[0]][new_pair[1]].append(new_single_class)
@@ -85,9 +104,11 @@ def generate_department_groups(classes_data, departments_data):
                     for single_value in values:
                         if single_class_for == keys:
                             new_single_class = single_class.copy()
-                            lecturer = new_single_class['Lecturer']
-                            number_of_lecturer = random.randrange(0, len(lecturer))
-                            new_single_class['Lecturer'] = [lecturer[number_of_lecturer]]
+                            # need to remove
+                            # lecturer = new_single_class['Lecturer']
+                            # number_of_lecturer = random.randrange(0, len(lecturer))
+                            # new_single_class['Lecturer'] = [lecturer[number_of_lecturer]]
+
                             new_single_class["for"] = [single_class_for]
                             if new_single_class['Subject'][-1] == "M":
                                 new_single_class['Group'] = single_value
@@ -96,7 +117,7 @@ def generate_department_groups(classes_data, departments_data):
                             departments[keys][single_value].append(new_single_class)
                             new_data.append(new_single_class)
 
-    return departments, new_data
+    return departments, new_data, depart_group
 
 
 def write_data(data, path):
@@ -104,17 +125,17 @@ def write_data(data, path):
         json.dump(data, write_file, indent=4)
 
 
-def write_data2(data, departments, path):
-    new_data = {'departments': departments, 'Classes': data}
+def write_data2(data, departments, depart_group, path):
+    new_data = {'departments': departments, 'department_groups': depart_group, 'Classes': data}
     with open(path, 'w') as write_file:
         json.dump(new_data, write_file, indent=4)
 
 
 if __name__ == "__main__":
     input_file = 'classes/1st iug_input v4 grouping_modules.json'
-    output_file = 'classes/department groups with modules.json'
-    output_file2 = 'classes/2nd iug_input v4 first_solution.json'
+    output_file = 'classes/department groups with modules v14.json'
+    output_file2 = 'classes/2nd iug_input v4 first_solution v14.json'
     da = load_data(input_file)
     departments_groups = generate_department_groups(da[0], da[1])
     write_data(departments_groups[0], output_file)
-    write_data2(departments_groups[1], da[1],  output_file2)
+    write_data2(departments_groups[1], da[1], departments_groups[2],  output_file2)
